1f495b14886e9a614762fed17470b55a9997fe97
Lee <leeb@lee-smith.org>
2021-06-20 17:51:17 -0400

    Add support for 'id' and 'class_list' keys for parents and leaves.

--- menumaker.py
+++ menumaker.py
@@ -8,15 +8,15 @@ from json.decoder import JSONDecodeError
 indent = 0
 
 # Leaves MUST contain 'text' and 'href' keys.
-# Leaves MAY contain 'id' and / or 'disabled' keys.
-# 'id', 'text', and 'href' must be strings. 'disabled' must be bool
+# Leaves MAY contain 'id' and / or 'class_list' keys.
+# 'id', 'text', and 'href' must be strings. 'class_list' must be list
 # Returns None if the node IS a leaf
 def isLeaf(node):
 
     types = {
         "id" : str,
         "href" : str,
-        "disabled": bool,
+        "class_list": list,
         "text": str,
     }
 
@@ -43,13 +43,16 @@ def isLeaf(node):
     return None
 
 # Parents MUST contain 'text' and 'nodes' keys.
-# 'text' must be str and 'nodes' must be list
+# Parents MAY contain 'id' and / or 'class_list' keys.
+# 'text' and 'id' must be str and 'nodes' and 'class_list' must be lists
 # Returns None if the node is a parent
 def isParent(node):
 
     types = {
         "text": str,
-        "nodes": list
+        "nodes": list,
+        "id": str,
+        "class_list": list
     }
 
     keyset = node.keys()
@@ -106,33 +109,43 @@ def validJSON(json):
 def makeMenu(json):
     global indent
 
-    def makeAnchor(txt, target, disabled=None, idstr=None):
-        
-        target = f' href="{target}"' if target else ''
-        disabled = f' class="disabled"' if disabled else ''
+    def makeAnchor(txt, class_list, idstr=None, href=None):
+       
+        classes = class_list and " ".join(class_list)
+        cls = f' class="{classes}"' if classes else ''
+        href = f' href="{href}"' if href else ''
         idstr = f' id="{idstr}"' if idstr else ''
-        html = f'<a{disabled}{idstr}{target}>{txt}</a>'
+        html = f'<a{cls}{idstr}{href}>{txt}</a>'
         return html
     
     # Leaf?
     if isLeaf(json) == None:
         indent += 1
-        disabled = None if 'disabled' not in json.keys() else \
-            json['disabled']
+        # id string and class_list possible
+        cls_list = None if 'class_list' not in json.keys() else \
+            json['class_list']
         idstr = None if 'id' not in json.keys() else \
             json['id']
         printWithIndent(makeAnchor(json['text'],
-            json['href'],
-            disabled,
-            idstr))
+            cls_list,
+            idstr,
+            json['href']))
         indent -= 1
 
     # Parent!
     else:
-        arrowClass = None if indent < 3 else (
-             'downarrow' if indent == 3 else  'rightarrow ')
+        arrowClass = [] if indent < 3 else \
+            (['downarrow'] if indent == 3 else  ['rightarrow'])
         
-        printWithIndent(makeAnchor(json['text'],arrowClass,None))
+        # id string and class_list possible
+        cls_list = [] if 'class_list' not in json.keys() else \
+            json['class_list']
+
+        classes = arrowClass + cls_list
+
+        idstr = None if 'id' not in json.keys() else \
+            json['id']
+        printWithIndent(makeAnchor(json['text'],classes, idstr))
         printWithIndent('<ul>')
         indent += 1
 
